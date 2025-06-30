09160b21fc941a3dc4150c1d024d0b25f5198c99
Himanshu Panwar <hpanwar875@gmail.com>
2025-06-30 23:33:04 +0530

    improve schema model

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.post("/signUp", async (req, res) => {
     await userObj.save();
     res.send(" user succesfully stored");
   } catch (error) {
-    res.status(400).send("error found");
+    res.status(400).send("error found:"+ error);
   }
 });
 
@@ -32,28 +32,50 @@ app.get("/user", async (req, res) => {
   } catch (error) {
     res.status(404).send("something went wrong");
   }
-
-
-  
 });
-
 
 // creating a api to getting all users/feed
 
-app.get("/feed", async (req,res) => {
+app.get("/feed", async (req, res) => {
   try {
-    const allUsers = await User.find({})
-    res.status(200).send(allUsers)
-    
+    const allUsers = await User.find({});
+    res.status(200).send(allUsers);
   } catch (error) {
-    res.status(404).send("something went wrong",);
-    console.log(error)
+    res.status(404).send("something went wrong");
+    console.log(error);
+  }
+});
+
+
+//creating api for  deleting a user
+app.delete('/user', async (req,res) => {
+  try {
+
+  // await User.deleteOne({age:'22'})
+  const deletedUser = await User.findOneAndDelete({ age: "22" })
+  console.log(deletedUser)
+    res.status(200).send("deleted successfully")
+  } catch (error) {
+    res.status(404).send("something went wrong")
   }
   
 })
 
-
-
+//creating api for updating user 
+app.patch('/user', async (req,res) => {
+  //this is the id of the user
+  const userId = req.body._id
+  //this is the data which user wants to update
+  const data = req.body
+  try {
+    const updatedUser = await User.findByIdAndUpdate({_id:userId},data,{returnDocument : "after"})
+    console.log(updatedUser)
+  
+    res.status(200).send("user updated")
+  } catch (error) {
+    res.status(500).send("something went wrong")
+  }
+})
 
 dbConnection()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,62 @@
 const mongoose = require("mongoose");
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
   firstName: {
-    type: "string",
+    type: String,
+    required: true,
+    trim: true,
+    minLength:2,
+    maxLength:12
   },
   lastName: {
-    type: "string",
+    type: String,
+    trim:true,
+       minLength:2,
+    maxLength:12
   },
   emailId: {
-    type: "string",
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+   validate: function isValidEmail (email){
+      return  validator.isEmail(email)
+    }
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate : function isStrongPassword(value){
+      return validator.isStrongPassword(value)
+    }
+
+  },
+  gender: {
+    type: String,
+    required:true,
+    enum:['male','female','other'],
+    lowercase:true,
+    trim:true,
+    },
   age: {
     type: Number,
+    required:true,
+    min:0,
+    max:100,
+    validator: Number.isInteger
   },
-});
+  about: {
+    type: String,
+    default:"default description hai"
+  },
+  photoUrl:{
+    type: String,
+    required: true
+  },
+},{timestamps:true});
 
 const User = mongoose.model("User", userSchema);
 
