08f88f8a66ffff80b2ed63dc0cf67eb2b09047d9
Himanshu Panwar <hpanwar875@gmail.com>
2025-07-07 23:05:00 +0530

    validating connection req

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const requestRouter = express.Router();
 const userAuth = require("../middleware/auth");
-const userConnectionReq = require("../models/userConnection")
+const userConnectionReq = require("../models/userConnection");
+const user = require("../models/user");
 
 requestRouter.post("/request/send/:status/:id", userAuth, async (req, res) => {
   try {
@@ -15,12 +16,26 @@ requestRouter.post("/request/send/:status/:id", userAuth, async (req, res) => {
       return res.status(200).send("invalid request Status!!");
 
     }
+   
+    
+    //validation for toUser is exist in ourDb then send connection
+    const toUser = await user.findById(toUserId)
+    if(!toUser){
+      return res.status(400).send("user not found!!")
+    }
+    
     const ConnectionRequest = new userConnectionReq({
       fromUserId,
       toUserId,
       status,
     });
 
+    // validaton for not send request yoursel.
+    if(ConnectionRequest.fromUserId === ConnectionRequest.fromUserId){
+      return res.status(200).send("You can't send connection to yourself.")
+    }
+
+    //if req(pending) person1 can't send req again to same person & person2 also can't send req to person 1
     const existingConnectionReq = await userConnectionReq.findOne({
       $or :[
         {fromUserId,toUserId},
