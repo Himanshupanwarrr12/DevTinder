104569987b6e0fa333128c9bd94379801e075b23
Himanshu Panwar <hpanwar875@gmail.com>
2025-07-07 17:25:14 +0530

    api improvement for not sending req to existing connectionReq

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const requestRouter = express.Router();
 const userAuth = require("../middleware/auth");
-const userConnection = require("../models/userConnection");
+const userConnectionReq = require("../models/userConnection")
 
 requestRouter.post("/request/send/:status/:id", userAuth, async (req, res) => {
   try {
@@ -10,19 +10,30 @@ requestRouter.post("/request/send/:status/:id", userAuth, async (req, res) => {
     const status = req.params.status;
 
     
-    const allowedReqStatus = ["intersted", "ignored"];
+    const allowedReqStatus = ["interested", "ignored"];
     if (!allowedReqStatus.includes(status)) {
       return res.status(200).send("invalid request Status!!");
 
     }
-    const ConnectionRequest = new userConnection({
+    const ConnectionRequest = new userConnectionReq({
       fromUserId,
       toUserId,
       status,
     });
 
+    const existingConnectionReq = await userConnectionReq.findOne({
+      $or :[
+        {fromUserId,toUserId},
+        {fromUserId:toUserId,toUserId:fromUserId}
+      ]
+    })
+    if(existingConnectionReq){
+      return res.status(400).send("Connection request already exist")
+    }
+    
 
     const Data = await ConnectionRequest.save();
+
     res.status(200).send(Data);
   } catch (error) {
     res.status(400).send("error found:" + error);
