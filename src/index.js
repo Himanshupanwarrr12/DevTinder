b5f851fc5e65f15f48f00cf4f177c917105b495b
Himanshu Panwar <hpanwar875@gmail.com>
2025-07-11 00:05:45 +0530

    added cors

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ const port = 7777;
 app.use(express.json());
 app.use(cookieParser());
 
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
+const cors = require("cors");
 
-const  authRouter = require('./routes/auth')
-const profileRouter = require('./routes/profile')
-const requestRouter = require('./routes/request')
-const userRouter = require("./routes/user")
-
-app.use('/',authRouter)
-app.use('/',profileRouter)
-app.use('/',requestRouter) 
-app.use('/',userRouter) 
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials:true
+  })
+);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 dbConnection()
   .then(() => {
