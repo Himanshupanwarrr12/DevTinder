0b56b9acd39a22e4d62bd7230a7eb71e9d758c11
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-13 21:20:54 +0530

    up

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://16.171.18.224:7777", 
+    origin:["*"], 
     credentials: true
   })
 );
@@ -20,6 +20,7 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
