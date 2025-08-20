5cfb4f623205dfb6287d7d8e802b0655df702792
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-20 07:40:33 +0530

    secure-db key

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   mongoose.connect(
-    "mongodb+srv://himanshudoes:Himanshu12@cluster0.cjcbcbp.mongodb.net/DevTinder?retryWrites=true&w=majority&appName=Cluster0"
+    "Dbkey"
   );
 };
 
